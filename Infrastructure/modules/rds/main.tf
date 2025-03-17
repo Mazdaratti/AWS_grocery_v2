a1580b7f4e094f0859d74a6a368b8620e326ebfd
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 09:03:47 +0100

    Updated rds module

--- Infrastructure/modules/rds/main.tf
+++ Infrastructure/modules/rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "rds" {
-  identifier             = var.identifier
+  identifier             = var.db_identifier
   snapshot_identifier    = var.snapshot_id
   instance_class         = var.instance_class
   allocated_storage      = var.allocated_storage
