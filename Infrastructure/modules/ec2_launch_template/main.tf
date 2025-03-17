5c6823fc7e53222aa3b139ccef6f3aa7567e5f7b
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 08:59:32 +0100

    launch template main updated

--- Infrastructure/modules/ec2_launch_template/main.tf
+++ Infrastructure/modules/ec2_launch_template/main.tf
@@ -23,68 +23,10 @@ resource "aws_launch_template" "grocery" {
   }
 
   # Embed the user data script directly
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              # Install CloudWatch Agent
-              sudo yum install -y amazon-cloudwatch-agent
-
-              # Configure the agent
-              cat <<EOC > /opt/aws/amazon-cloudwatch-agent/bin/config.json
-              {
-                "logs": {
-                  "logs_collected": {
-                    "files": {
-                      "collect_list": [
-                        {
-                          "file_path": "/var/lib/docker/containers/**/*-json.log",
-                          "log_group_name": "/app/docker-logs",
-                          "log_stream_name": "{instance_id}",
-                          "timezone": "UTC"
-                        }
-                      ]
-                    }
-                  }
-                },
-                "metrics": {
-                  "metrics_collected": {
-                    "cpu": {
-                      "measurement": [
-                        "cpu_usage_idle",
-                        "cpu_usage_user",
-                        "cpu_usage_system"
-                      ],
-                      "resources": [
-                        "*"
-                      ],
-                      "totalcpu": true
-                    },
-                    "disk": {
-                      "measurement": [
-                        "used_percent",
-                        "inodes_free"
-                      ],
-                      "resources": [
-                        "/"
-                      ]
-                    },
-                    "mem": {
-                      "measurement": [
-                        "mem_used_percent"
-                      ]
-                    },
-                    "net": {
-                      "measurement": [
-                        "bytes_sent",
-                        "bytes_recv"
-                      ]
-                    }
-                  }
-                }
-              }
-              EOC
-
-              # Start the CloudWatch Agent
-              sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
-              EOF
-  )
+  user_data = base64encode(templatefile("${path.module}/user_data.sh.tpl", {
+    region             = var.region
+    ecr_repository_url = var.ecr_repository_url
+    image_tag          = var.image_tag
+    ecr_domain         = split("/", var.ecr_repository_url)[0]
+  }))
 }
