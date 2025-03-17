c08514bedcc27e58a0c85528428b4cd17af0fa81
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 09:04:14 +0100

    Variables for rds module added

--- Infrastructure/modules/rds/outputs.tf
+++ Infrastructure/modules/rds/outputs.tf
@@ -7,3 +7,18 @@ output "rds_id" {
   description = "The ID of the RDS instance"
   value       = aws_db_instance.rds.id
 }
+
+output "rds_az" {
+  description = "The Availability Zone of the RDS instance"
+  value       = aws_db_instance.rds.availability_zone
+}
+
+output "rds_arn" {
+  description = "The ARN of the RDS instance"
+  value       = aws_db_instance.rds.arn
+}
+
+output "rds_host" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.rds.address
+}
