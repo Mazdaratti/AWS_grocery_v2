ecae4d19180b1daecb2bc165b3622b37b208dfae
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 09:03:08 +0100

    Outputs for lambda_module created

--- Infrastructure/modules/lambda/outputs.tf
+++ Infrastructure/modules/lambda/outputs.tf
@@ -1,4 +1,14 @@
-output "lambda_role_arn" {
-  description = "The ARN of the IAM role for Lambda"
-  value       = aws_iam_role.lambda_role.arn
+output "lambda_function_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.db_populator.function_name
+}
+
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function."
+  value       = aws_lambda_function.db_populator.arn
+}
+
+output "sfn_arn" {
+  description = "The ARN of the Step Functions state machine."
+  value       = aws_sfn_state_machine.db_restore_sfn.arn
 }
