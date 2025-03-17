d06e13163b67d54cb60aa6ef3ba19cc51ef0fa84
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 08:58:30 +0100

    Deleted cloudwatch_logging module

--- Infrastructure/modules/cloudwatch_logging/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "log_group_name" {
-  description = "Name of the CloudWatch Log Group"
-  value       = aws_cloudwatch_log_group.app_logs.name
-}
-
--- Infrastructure/modules/cloudwatch_logging/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "ec2_role_name" {
-  description = "Name of the IAM role to attach the CloudWatchAgentServerPolicy"
-  type        = string
-}
