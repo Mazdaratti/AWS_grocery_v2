773a088f67aff48a4c9e25df7a96463533d01927
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-17 09:07:22 +0100

    Outputs for sg module updated

--- Infrastructure/modules/security_groups/outputs.tf
+++ Infrastructure/modules/security_groups/outputs.tf
@@ -12,3 +12,8 @@ output "rds_security_group_id" {
   value = aws_security_group.rds_sg.id
   description = "The ID of the RDS security group"
 }
+
+output "lambda_security_group_id" {
+  value = aws_security_group.lambda_sg.id
+  description = "The ID of the Lambda security group"
+}
