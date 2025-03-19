01d01246019bc0ea45e08b700302fe81cb5abd4e
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-20 00:50:16 +0100

    Provider version added
    Provider version added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.85.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "eu-central-1"
-  #profile = "default"
+  region = var.region
 }
 
 # ECR Repositories
